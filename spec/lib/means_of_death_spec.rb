25f44668636d7f0a04ed0515a58ce7fc6cc1bf41
Werter Meira <wertermeira@gmail.com>
2021-11-29 09:59:09 -0300

    verify and cclean code with rubocop

--- spec/lib/means_of_death_spec.rb
+++ spec/lib/means_of_death_spec.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe MeansOfDeath do
   let(:means_of_death) { described_class.new.means_list }
   describe '.means_list' do
-    File.open(File.expand_path('../../../logs/means_of_death.txt', __FILE__), 'r').each_line do |line|
+    File.open(File.expand_path('../../logs/means_of_death.txt', __dir__), 'r').each_line do |line|
       context " when include mean #{line.strip}" do
         it { expect(means_of_death).to include(line.strip.to_sym) }
       end
--- spec/lib/read_log_spec.rb
+++ spec/lib/read_log_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe ReadLog do
   let(:file_test) { '../../logs/game_test.log' }
-  describe '.call' do
+  describe '.lines' do
     context 'when count lines' do
       it { expect(described_class.new(file_test).lines.count).to eq(158) }
     end
